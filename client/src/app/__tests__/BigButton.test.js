fdf6d86f36d658dc0600452e267ac6892b8cbe45
Paja777 <pavle.rvovic95@gmail.com>
2024-01-17 14:08:46 +0100

    testing

--- client/src/app/__tests__/BigButton.test.js
+++ client/src/app/__tests__/BigButton.test.js
@@ -1,5 +1,5 @@
 import { screen } from "@testing-library/react";
-import BigButton from "./BigButton";
+import BigButton from "../components/BigButton";
 import { renderWithProviders } from "../utils/utils-for-test";
 
 test("icon should be rendered", () => {
@@ -7,3 +7,9 @@ test("icon should be rendered", () => {
     const iconEl = screen.getByTestId("icon");
     expect(iconEl).toBeInTheDocument();
   });
+
+  test("text element should be rendered", () => {
+    renderWithProviders(<BigButton path="/" title="Search" />);
+    const textEl = screen.getByTestId("text");
+    expect(textEl).toBeInTheDocument();
+  })
