a022528a69501f2f21c08fefadad3d80fbbf3863
Paja777 <pavle.rvovic95@gmail.com>
2024-02-17 19:42:52 +0100

    Create axios logic to contact api

---
+++ client/src/app/api/agent.ts
@@ -0,0 +1,15 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+
+const sleep = () => new Promise(resolve => setTimeout(resolve, 500));
+
+axios.defaults.baseURL = 'http://localhost:5000/';
+axios.defaults.withCredentials = true;
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+    get: (url: string, params?: URLSearchParams) => axios.get(url, {params}).then(responseBody),
+    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+    put: (url: string, body: {}) => axios.patch(url, body).then(responseBody),
+    delete: (url: string) => axios.delete(url).then(responseBody),
+}
