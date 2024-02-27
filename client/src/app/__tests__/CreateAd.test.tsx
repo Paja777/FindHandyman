ebaa4bc510d9858c416354ded12e1aa2595d6364
Paja777 <pavle.rvovic95@gmail.com>
2024-02-27 17:33:24 +0100

    Testing async button enableing

--- client/src/app/__tests__/CreateAd.test.tsx
+++ client/src/app/__tests__/CreateAd.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, screen } from "@testing-library/react";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { renderWithProviders } from "../utils/utils-for-test";
 import CreateAd from "../features/roleHandyman/CreateAd";
@@ -17,24 +17,22 @@ describe("CrateAd", () => {
     const describeTextElement = screen.getByPlaceholderText("Describe your services (maximum 300 caracters)");
     expect(describeTextElement).toBeInTheDocument();
     
-    
     fireEvent.change(describeTextElement, {target : { value: "aloooooooooooooooooooooooooooooooooooooooooooooooooooooo"}});
     expect(describeTextElement).toHaveValue('aloooooooooooooooooooooooooooooooooooooooooooooooooooooo')
   });
-  // it("should be enabled after adding service", () => {
-    // const buttonEl = screen.getByRole("button", {name: /Create/i});
-    // const serviceTextElements = screen.getByPlaceholderText(/service name1/i);
-    // const priceTextElements = screen.getByPlaceholderText(/service price1/i);
-    // const describeTextElements = screen.getByTestId("describe");
-    // expect(priceTextElements).toBeInTheDocument()
-    // expect(serviceTextElements).toBeInTheDocument()
-    // expect(describeTextElements).toBeInTheDocument()
-    // fireEvent.change(serviceTextElements, {target : { value: "Pranje"}});
-    // expect(serviceTextElements).toHaveValue('Pranje')
-    // fireEvent.change(priceTextElements, {target : { value: "$100"}});
-    // expect(priceTextElements).toHaveValue('$100')
-    // fireEvent.change(describeTextElements, {target : { value: "aloooooooooooooooooooooooooooooooooooooooooooooooooooooo"}});
-    // expect(describeTextElements).toHaveValue('aloooooooooooooooooooooooooooooooooooooooooooooooooooooo')
-    // expect(buttonEl).toBeDisabled();
-  // });
+  it("should be enabled after adding service", async() => {
+    const buttonEl = screen.getByRole("button", {name: /Create/i});
+    const serviceTextElement = screen.getByPlaceholderText(/service name1/i);
+    const priceTextElement = screen.getByPlaceholderText(/service price1/i);
+    expect(priceTextElement).toBeInTheDocument()
+    expect(serviceTextElement).toBeInTheDocument()
+    fireEvent.change(serviceTextElement, {target : { value: "Pranje"}});
+    expect(serviceTextElement).toHaveValue('Pranje')
+    fireEvent.change(priceTextElement, {target : { value: "$100"}});
+    expect(priceTextElement).toHaveValue('$100')
+    // wait for button to become enabled
+    await waitFor(() => {
+      expect(buttonEl).toBeEnabled();
+    });
+  });
 });
