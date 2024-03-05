4ec14c3e7a7dd1360fc0517fd5198fa18e5c6efd
Paja777 <pavle.rvovic95@gmail.com>
2024-03-05 13:28:54 +0100

    Testing with mocked server data

---
+++ client/src/app/__tests__/AdList.test.tsx
@@ -0,0 +1,22 @@
+// import AdsList from "../components/AdsList";
+// import { renderWithProviders } from "../utils/utils-for-test";
+// import {
+//   fireEvent,
+//   getByText,
+//   render,
+//   screen,
+//   waitFor,
+// } from "@testing-library/react";
+
+// describe("AdsList", () => {
+//   beforeEach(() => {
+//     renderWithProviders(<AdsList ads={[]} />);
+//   });
+
+//   it("should render ad on app init", async () => {
+//     const ads = await screen.findByText('');
+    
+//   });
+
+  
+// });
--- client/src/mocks/handlers.ts
+++ client/src/mocks/handlers.ts
@@ -1,7 +1,24 @@
 import { rest } from 'msw'
 
 export const handlers = [
-    rest.get("http://localhost:5000/", (req, res, ctx) => {
-        return
+    rest.get("http://localhost:5000/ad", (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json([
+                {
+                    _id: 'sdfsefsdf343',
+    name: 'Pavle',
+    category: 'programiranje',
+    services: [{programiranje: "$1000"}],
+    images: [],
+    rating: 5,
+    description: 'Bla bla bla bla',
+    note: '',
+    adRole: "Handyman",
+    user_id: 'asdasdas1234',
+    adCreatorRating: 5,
+                }
+            ])
+        )
     })
 ]
